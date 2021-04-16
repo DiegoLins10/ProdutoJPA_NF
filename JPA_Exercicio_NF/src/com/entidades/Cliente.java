3bb068d8454e50d086a7e2bef3fef1569ec35caf
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-16 09:40:18 -0300

    entidades cliente/item/produto

---
+++ JPA_Exercicio_NF/src/com/entidades/Cliente.java
@@ -0,0 +1,56 @@
+package com.entidades;
+/*
+ * @Author Diego Lins 16/04
+ * entidade Cliente
+ */
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity // crtl + shift + o pegar classe
+public class Cliente {
+	@Id
+	private int id;
+	private String nome;
+	private String email;
+
+	public Cliente() {
+
+	}
+
+	public Cliente(int id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+
+}
---
+++ JPA_Exercicio_NF/src/com/entidades/Produto.java
@@ -0,0 +1,57 @@
+package com.entidades;
+/*
+ * @Author Diego Lins 16/04
+ * entidade produto
+ */
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity                                //CTRL + SHIFT + O importa todas as classe
+public class Produto {
+	@Id
+	private int id;
+	private String nome;
+	private float precoUnitario;
+
+	public Produto() {
+
+	}
+
+	public Produto(int id, String nome, float precoUnitario) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.precoUnitario = precoUnitario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(float precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", precoUnitario=" + precoUnitario + "]";
+	}
+
+}
